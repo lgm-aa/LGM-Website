e690361b38ff22b3bd02a12b7220ab7f88b315ad
Tae Hoon Kim <taehoonkim@Tae-Hoons-MacBook-Pro.local>
2025-06-20 10:53:35 -0400

    home page improvements

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,27 +8,21 @@ import "./Home.css";
 export default function Home() {
   return (
     <main className="landing-page">
-      {/* Section with background image */}
-      <div className="section-with-background">
-        <AnimatedSection className="content-block" delay={100}>
+      {/* Section with background image via inline style only */}
+      <div
+        className="section-with-background"
+        style={{ backgroundImage: "url('/lgm_easter.jpg')" }}
+      >
+        <AnimatedSection className="content-block" delay={400}>
           <h1>Living Grace Ministry</h1>
           <p>
             Making a community of Jesus’s disciples who love like Jesus through
             God’s grace
           </p>
-          <Link to="/about/our_story" className="dropdown-item">
-            <button>Learn More</button>
-          </Link>
         </AnimatedSection>
 
-        <AnimatedSection className="content-block" delay={100}>
-          <h1>Sunday Service</h1>
-          <p>
-            Join us for snacks and fellowship at 12:45 pm. Service starts at
-            1:30 pm in the KUMCAA building, next door to our previous building
-          </p>
-
-          <Link to="/about/sunday_service" className="dropdown-item">
+        <AnimatedSection className="content-block only-button" delay={400}>
+          <Link to="/about/our_story" className="button-link">
             <button>Learn More</button>
           </Link>
         </AnimatedSection>
