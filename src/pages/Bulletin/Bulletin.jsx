09ed24fbdf915c1ce4cfc58bd798e83d1f0d79af
Tae Hoon Kim <taehoonkim@Tae-Hoons-MacBook-Pro.local>
2025-06-27 16:40:54 -0400

    Capital letters

--- src/pages/Bulletin/Bulletin.jsx
+++ src/pages/Bulletin/Bulletin.jsx
@@ -1,7 +1,7 @@
 // src/pages/Bulletin.jsx
 import React from "react";
 import LatestBulletin from "../../components/LatestBulletin";
-import "./bulletin.css"; // Adjust path if necessary
+import "./Bulletin.css"; // Adjust path if necessary
 
 export default function Bulletin() {
   return (
